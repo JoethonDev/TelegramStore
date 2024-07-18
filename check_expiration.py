ab70dbb504212ffe4b3bb5fd5512e197b4862a86
George Zakhary <elprincedoca@gmail.com>
2024-07-18 13:29:56 +0300

    add scheduled task

---
+++ check_expiration.py
@@ -0,0 +1,46 @@
+import psycopg2
+import logging
+
+def is_expired(date):
+    return False
+
+try:
+    connection = psycopg2.connect(
+                database="manager_builder",
+                user="manager_builder_owner",
+                password="QjbXi9gDNpP7",
+                host="ep-delicate-tooth-a2u3cnv5.eu-central-1.aws.neon.tech",
+                port="5432"
+            )
+except Exception as e:
+    logging.exception("Connection Error!")
+    logging.exception(e)
+
+cursor = connection.cursor()
+# Get bots
+bots = []
+try:
+    query = "SELECT id, expiration_date FROM bots"
+    cursor.execute(query)
+    bots = cursor.fetchall()
+except Exception as e:
+    logging.exception("Fetching Bots Error!")
+    logging.exception(e)
+
+# Check Date
+expired_bots = list()
+for bot in bots:
+    if is_expired(bot[1]):
+        expired_bots.append(bot[0])
+
+expired_bots = ",".join(expired_bots)
+
+try:
+    query = f"UPDATE bots SET status='stopped' WHERE id IN ({expired_bots})"
+    cursor.execute(query)
+except Exception as e:
+    logging.exception("Updating Bots Error!")
+    logging.exception(e)
+
+connection.commit()
+connection.close()
